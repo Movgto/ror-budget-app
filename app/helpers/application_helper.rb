4375d405bf095d573d1caf863f3b36e9a56a440b
Gumaro Monroy <maromvz@gmail.com>
2023-12-02 01:20:53 -0600

    Add styles according to project guidelines

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def nav_title(name)
+    puts 'Action name:', name
+    case name
+    when 'categories'
+      'CATEGORIES'
+    when 'operations'
+      'TRANSACTIONS'
+    when 'sessions'
+      'LOGIN'
+    when 'registrations'
+      'SIGNUP'
+    end
+  end
 end
