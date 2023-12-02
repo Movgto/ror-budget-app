b5ada2f428314b2a0d72e4394f712c20b6bdc7c3
Gumaro Monroy <maromvz@gmail.com>
2023-12-02 02:52:34 -0600

    Add request tests

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,7 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe "Categories", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { TestAssetBuilder.user }
+  describe 'GET categories#index' do
+    before do
+      sign_in user
+      get '/categories'
+    end
+
+    it 'Returns http success' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'Should render the template recipe/index' do
+      expect(response).to render_template('categories/index')
+    end
+
+    it 'Body should include categories title' do
+      expect(response.body).to match(/CATEGORIES/)
+    end
+  end
+
+  describe 'GET categories#new' do
+    before do
+      sign_in user
+      get '/categories/new'
+    end
+
+    it 'Returns http success' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'Should render the template recipe/new' do
+      expect(response).to render_template('categories/new')
+    end
   end
 end
--- spec/requests/operations_spec.rb
+++ spec/requests/operations_spec.rb
@@ -1,7 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe "Operations", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { TestAssetBuilder.user }
+  let(:category) { Category.create(user: user, name: 'A store') }
+  let(:operation) { Operation.create(user: user, name: 'TV 40', amount: 100) }
+  let(:operation_category) { CategoryOperation.new(category: category, operation: operation) }
+
+  describe 'GET operations#index' do
+    before do
+      sign_in user
+      operation_category.save
+      get "/categories/#{category.id}/operations"
+    end
+
+    it 'Returns http success' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'Should render the template operations/index' do
+      expect(response).to render_template('operations/index')
+    end
+
+    it 'Body should include the operation name' do
+      puts 'Operations:', category.operations
+      expect(response.body).to match(/TV 40/)
+    end
+  end
+
+  describe 'GET operations#new' do
+    before do
+      sign_in user
+      get "/categories/#{category.id}/operations/new"
+    end
+
+    it 'Returns http success' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'Should render the template recipe/new' do
+      expect(response).to render_template('operations/new')
+    end
   end
 end
